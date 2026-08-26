068f03772e51577782844ee0efed4b1a07d50dbb
maaitlunghau <chunhau.py@gmail.com>
2026-08-26 14:47:07 +0700

    feat: initialize new project with Dart

---
+++ standard_demo_dart/bin/standard_demo_dart.dart
@@ -0,0 +1,5 @@
+import 'package:standard_demo_dart/standard_demo_dart.dart' as standard_demo_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${standard_demo_dart.calculate()}!');
+}
---
+++ standard_demo_dart/test/standard_demo_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:standard_demo_dart/standard_demo_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
